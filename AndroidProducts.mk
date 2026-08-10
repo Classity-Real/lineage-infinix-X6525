601059e4b93a552b721b8c7f8719883c7d47a72c
Mountain <uiportsarchive@gmail.com>
2026-08-10 16:26:26 +0800

    Update COMMON_LUNCH_CHOICES in AndroidProducts.mk
    Removed PRODUCT_MAKEFILES entry for lineage_Infinix-X6525.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -7,9 +7,6 @@ PRODUCT_MAKEFILES := \
 COMMON_LUNCH_CHOICES := \
     lineage_Infinix-X6525-userdebug
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_Infinix-X6525.mk
-
 COMMON_LUNCH_CHOICES := \
     lineage_Infinix-X6525-user \
     lineage_Infinix-X6525-userdebug \
