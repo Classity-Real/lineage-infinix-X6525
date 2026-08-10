217598a10a5c7f5785f309fee1b409513ea4849d
Mountain <uiportsarchive@gmail.com>
2026-08-10 17:06:05 +0800

    Attempting Again

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,2 @@
-# Copyright (C) 2026 The Android Open Source Project
-# SPDX-License-Identifier: Apache-2.0
-
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_Infinix-X6525.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_Infinix-X6525-userdebug
-
-COMMON_LUNCH_CHOICES := \
-    lineage_Infinix-X6525-user \
-    lineage_Infinix-X6525-userdebug \
-    lineage_Infinix-X6525-eng
+    $(LOCAL_DIR)/lineage_X6525.mk
