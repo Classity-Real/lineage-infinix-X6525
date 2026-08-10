c5a0ce742564795232f4e1c37c7ca25ae8377ccd
Mountain <uiportsarchive@gmail.com>
2026-08-10 16:25:37 +0800

    Change product makefiles and lunch choices to lineage
    Updated product makefiles and lunch choices to reflect lineage.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,16 @@
-#
-# Copyright (C) 2025 The Android Open Source Project
-# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
-#
+# Copyright (C) 2026 The Android Open Source Project
 # SPDX-License-Identifier: Apache-2.0
-#
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_Infinix-X6525.mk
+    $(LOCAL_DIR)/lineage_Infinix-X6525.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_Infinix-X6525-user \
-    omni_Infinix-X6525-userdebug \
-    omni_Infinix-X6525-eng
+    lineage_Infinix-X6525-userdebug
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_Infinix-X6525.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_Infinix-X6525-user \
+    lineage_Infinix-X6525-userdebug \
+    lineage_Infinix-X6525-eng
